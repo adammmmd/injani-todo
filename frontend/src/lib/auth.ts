6d68cdd8a10bbf1bb0b2532041022ffae4beb5fd
adam.muhammad <adam.muhammad@cs.ui.ac.id>
2026-05-31 21:41:23 +0700

    update db

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,24 +1,33 @@
 import { betterAuth } from "better-auth";
 import { kyselyAdapter } from "@better-auth/kysely-adapter";
+import { Kysely } from "kysely";
+import { LibsqlDialect } from "@libsql/kysely-libsql";
 import Sqlite from "better-sqlite3";
-import { Kysely, SqliteDialect } from "kysely";
+import { SqliteDialect } from "kysely";
 import path from "path";
-const dbPath = path.join(process.cwd(), "auth.db");
-console.log("DB PATH:", dbPath);
 
-const db = new Kysely({
-  dialect: new SqliteDialect({
-    database: new Sqlite(path.join(process.cwd(), "auth.db")),
-  }),
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+const db = isProduction
+  ? new Kysely({
+      dialect: new LibsqlDialect({
+        url: process.env.TURSO_DATABASE_URL!,
+        authToken: process.env.TURSO_AUTH_TOKEN!,
+      }),
+    })
+  : new Kysely({
+      dialect: new SqliteDialect({
+        database: new Sqlite(path.join(process.cwd(), "auth.db")),
+      }),
+    });
 
 export const auth = betterAuth({
   database: kyselyAdapter(db, { type: "sqlite" }),
   secret: process.env.BETTER_AUTH_SECRET!,
   trustedOrigins: [
-     "http://localhost:3000",
-     "https://injani-todo.vercel.app",
-   ],
+    "http://localhost:3000",
+    process.env.NEXT_PUBLIC_APP_URL!,
+  ],
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
